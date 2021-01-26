fc249ffe9f06ca38af0702ff876f89e9f2803a31
mahan <m.mahmodi1380@gmail.com>
2021-01-26 10:07:31 -0500

    beta2road

--- game.cpp
+++ game.cpp
@@ -95,6 +95,37 @@ int main(){
     // sf::Texture truck1tex;
     //truck1tex.loadFromFile("res/img/police-car.png");
     truck32.setTexture(&truck1tex);
+ 
+ 
+
+
+    sf::RectangleShape truck13(sf::Vector2f(60.0f, 30.0f));
+    truck13.setPosition(sf::Vector2f(truck13.getPosition().x-480, window.getSize().y/2-truck13.getSize().y + 80.0f));
+    //sf::Texture truck1tex;
+    //truck1tex.loadFromFile("res/img/police-car.png");
+    truck13.setTexture(&truck1tex);
+    //car1 arrange
+    sf::RectangleShape car13(sf::Vector2f(60.0f, 30.0f));
+    car13.setPosition(sf::Vector2f(window.getSize().x-car13.getSize().x+480, window.getSize().y/2-car13.getSize().y+120.0f));
+    car13.setTexture(&truck1tex);
+    //truck2 arrange
+    sf::RectangleShape truck23(sf::Vector2f(60.0f, 30.0f));
+    truck23.setPosition(sf::Vector2f(truck23.getPosition().x-480, window.getSize().y/2-truck23.getSize().y + 160.0f));
+    //sf::Texture truck1tex;
+    //truck1tex.loadFromFile("res/img/police-car.png");
+    truck23.setTexture(&truck1tex);
+    //car2 arrange
+    sf::RectangleShape car23(sf::Vector2f(60.0f, 30.0f));
+    car23.setPosition(sf::Vector2f(window.getSize().x-car23.getSize().x+480, window.getSize().y/2-car23.getSize().y+200.0f));
+    car23.setTexture(&truck1tex);
+    //truck3 arrange
+    sf::RectangleShape truck33(sf::Vector2f(60.0f, 30.0f));
+    truck33.setPosition(sf::Vector2f(truck33.getPosition().x-480, window.getSize().y/2-truck33.getSize().y + 240.0f));
+    //sf::Texture truck1tex;
+    //truck1tex.loadFromFile("res/img/police-car.png");
+    truck33.setTexture(&truck1tex);
+
+    
 
 
     
@@ -145,8 +176,9 @@ int main(){
             {
                 window.close();
             }
+
             car1.move(-0.2,0);
-          if(car1.getPosition().x <(0-car1.getSize().x))
+            if(car1.getPosition().x <(0-car1.getSize().x))
             {
                 car1.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car1.getSize().y + 120.0f));
             }
@@ -154,7 +186,8 @@ int main(){
             {
                 window.close();
             }
-truck2.move(0.15,0);
+
+            truck2.move(0.18,0);
             if(truck2.getPosition().x > window.getSize().x)
             {
                 truck2.setPosition(sf::Vector2f(-1 * truck2.getSize().x, window.getSize().y/2-truck2.getSize().y + 160.0f));
@@ -163,7 +196,8 @@ truck2.move(0.15,0);
             {
                 window.close();
             }
- truck3.move(0.15,0);
+
+            truck3.move(0.13,0);
             if(truck3.getPosition().x > window.getSize().x)
             {
                 truck3.setPosition(sf::Vector2f(-1 * truck3.getSize().x, window.getSize().y/2-truck3.getSize().y + 240.0f));
@@ -172,8 +206,9 @@ truck2.move(0.15,0);
             {
                 window.close();
             }
- car2.move(-0.2,0);
-           if(car2.getPosition().x <(0-car2.getSize().x))
+
+            car2.move(-0.25,0);
+            if(car2.getPosition().x <(0-car2.getSize().x))
             {
                 car2.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car2.getSize().y + 200.0f));
             }
@@ -181,7 +216,8 @@ truck2.move(0.15,0);
             {
                 window.close();
             }
- truck11.move(0.15,0);
+
+            truck11.move(0.15,0);
             if(truck11.getPosition().x > window.getSize().x)
             {
                 truck11.setPosition(sf::Vector2f(-1 * truck11.getSize().x, window.getSize().y/2-truck11.getSize().y + 80.0f));
@@ -190,7 +226,8 @@ truck2.move(0.15,0);
             {
                 window.close();
             }
- truck21.move(0.15,0);
+            
+            truck21.move(0.18,0);
             if(truck21.getPosition().x > window.getSize().x)
             {
                 truck21.setPosition(sf::Vector2f(-1 * truck21.getSize().x, window.getSize().y/2-truck21.getSize().y + 160.0f));
@@ -199,7 +236,8 @@ truck2.move(0.15,0);
             {
                 window.close();
             }
- truck31.move(0.15,0);
+            
+            truck31.move(0.13,0);
             if(truck31.getPosition().x > window.getSize().x)
             {
                 truck31.setPosition(sf::Vector2f(-1 * truck31.getSize().x, window.getSize().y/2-truck31.getSize().y + 240.0f));
@@ -208,8 +246,9 @@ truck2.move(0.15,0);
             {
                 window.close();
             }
- car11.move(-0.2,0);
-           if(car11.getPosition().x <(0-car11.getSize().x))
+            
+            car11.move(-0.2,0);
+            if(car11.getPosition().x <(0-car11.getSize().x))
             {
                 car11.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car11.getSize().y + 120.0f));
             }
@@ -217,7 +256,8 @@ truck2.move(0.15,0);
             {
                 window.close();
             }
- car21.move(-0.2,0);
+            
+            car21.move(-0.25,0);
             if(car21.getPosition().x <(0-car21.getSize().x))
             {
                 car21.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car21.getSize().y + 200.0f));
@@ -229,9 +269,110 @@ truck2.move(0.15,0);
 
 
 
-        
-            window.clear();
 
+             truck12.move(0.15,0);
+            if(truck12.getPosition().x > window.getSize().x)
+            {
+                truck12.setPosition(sf::Vector2f(-1 * truck12.getSize().x, window.getSize().y/2-truck12.getSize().y + 80.0f));
+            }
+            if(frog.getGlobalBounds().intersects(truck12.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            car12.move(-0.2,0);
+            if(car12.getPosition().x <(0-car12.getSize().x))
+            {
+                car12.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car12.getSize().y + 120.0f));
+            }
+            if(frog.getGlobalBounds().intersects(car12.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            truck22.move(0.18,0);
+            if(truck22.getPosition().x > window.getSize().x)
+            {
+                truck22.setPosition(sf::Vector2f(-1 * truck22.getSize().x, window.getSize().y/2-truck22.getSize().y + 160.0f));
+            }
+            if(frog.getGlobalBounds().intersects(truck22.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            truck32.move(0.13,0);
+            if(truck32.getPosition().x > window.getSize().x)
+            {
+                truck32.setPosition(sf::Vector2f(-1 * truck32.getSize().x, window.getSize().y/2-truck32.getSize().y + 240.0f));
+            }
+            if(frog.getGlobalBounds().intersects(truck32.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            car22.move(-0.25,0);
+            if(car22.getPosition().x <(0-car22.getSize().x))
+            {
+                car22.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car22.getSize().y + 200.0f));
+            }
+            if(frog.getGlobalBounds().intersects(car22.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+
+             truck13.move(0.15,0);
+            if(truck13.getPosition().x > window.getSize().x)
+            {
+                truck13.setPosition(sf::Vector2f(-1 * truck13.getSize().x, window.getSize().y/2-truck13.getSize().y + 80.0f));
+            }
+            if(frog.getGlobalBounds().intersects(truck13.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            car13.move(-0.2,0);
+            if(car13.getPosition().x <(0-car13.getSize().x))
+            {
+                car13.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car13.getSize().y + 120.0f));
+            }
+            if(frog.getGlobalBounds().intersects(car13.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            truck23.move(0.18,0);
+            if(truck23.getPosition().x > window.getSize().x)
+            {
+                truck23.setPosition(sf::Vector2f(-1 * truck23.getSize().x, window.getSize().y/2-truck23.getSize().y + 160.0f));
+            }
+            if(frog.getGlobalBounds().intersects(truck23.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            truck33.move(0.13,0);
+            if(truck33.getPosition().x > window.getSize().x)
+            {
+                truck33.setPosition(sf::Vector2f(-1 * truck33.getSize().x, window.getSize().y/2-truck33.getSize().y + 240.0f));
+            }
+            if(frog.getGlobalBounds().intersects(truck33.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+            car23.move(-0.25,0);
+            if(car23.getPosition().x <(0-car23.getSize().x))
+            {
+                car23.setPosition(sf::Vector2f(window.getSize().x, window.getSize().y/2-car23.getSize().y + 200.0f));
+            }
+            if(frog.getGlobalBounds().intersects(car23.getGlobalBounds()))
+            {
+                window.close();
+            }
+
+
+            window.clear();
             // object drawing
             window.draw(frog);
             window.draw(truck1);
@@ -249,6 +390,11 @@ truck2.move(0.15,0);
             window.draw(truck22);
             window.draw(car22);
             window.draw(truck32);
+            window.draw(truck13);
+            window.draw(car13);
+            window.draw(truck23);
+            window.draw(car23);
+            window.draw(truck33);
             window.display();
 
          
